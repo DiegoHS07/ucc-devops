db7edd7096c7103a4687cd090cfbb70d34343610
Diego Alejandro Higuera Sierra <dhiguera09@gmail.com>
2021-10-14 19:56:50 -0500

    Actualización del .cpp para manejarlo desde Jenkins

--- my_program.cpp
+++ my_program.cpp
@@ -4,19 +4,43 @@
 
 using namespace std;
 
+int areaCirculo(double radio) {
+	double PI = 3.14159265358979323846;
+	double area = 0.0;
+	cout << "El radio del circulo es: " << radio << endl;
+	area = pow(radio, 2) * PI;
+	cout << "El area del ciculo es: " << area << endl;
+	cout << "Diego Alejandro Higuera Sierra" << endl;
+	return 0;
+}
+
+int areaTriangulo(double base, double altura) {
+	double area = 0.0;
+	cout << "La base del triangulo es: " << base << endl;
+	cout << "La altura del triangulo es: " << altura << endl;
+	area = (base * altura) / 2;
+	cout << "El area del triangulo es: " << area << endl;
+	cout << "Diego Alejandro Higuera Sierra" << endl;
+	return 0;
+}
+
+int areaCuadrado(double lado) {
+	double area = 0.0;
+	cout << "El lado del cuadrado es: " << lado << endl;
+	area = lado * lado;
+	cout << "El area del cuadrado es: " << area << endl;
+	cout << "Diego Alejandro Higuera Sierra" << endl;
+	return 0;
+}
+
 int main() {
-
-	int ivalor = 0;
-	int icontador = 0;
-
-	cout << "Ingrese un valor para calcular una suma: ";
-	cin >> ivalor;
-	cout << "Usted ingreso: " << ivalor << endl;
-	for(int i = 1; i <= ivalor; i++){
-		icontador += i;
-	}
-
-	cout << "El resultado es: " << icontador << endl;
-
+	cout << "*************** Calculadora de areas *************" << endl;
+	cout << "";
+	cout << "AREA DEL CIRCULO" << endl;
+	areaCirculo(8);
+	cout << "AREA DEL TRIANGULO" << endl;
+	areaTriangulo(2,6);
+	cout << "AREA DEL CUADRADO" << endl;
+	areaCuadrado(12);
 	return 0;
 }
